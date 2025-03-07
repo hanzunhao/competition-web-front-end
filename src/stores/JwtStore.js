ce16ab822113c6d96ff9c0e1c803033fb8fbc262
hzh <hanzunhao2004@163.com>
2025-03-07 20:57:39 +0800

    修复了完善登录功能带来的bug

--- src/stores/JwtStore.js
+++ src/stores/JwtStore.js
@@ -3,14 +3,5 @@ import { defineStore } from 'pinia';
 export const JwtStore = defineStore('JwtStore', {
     state: () => ({
         jwt: null
-    }),
-    persist: {
-        enabled: true, // 启用持久化
-        strategies: [
-            {
-                key: 'jwt-store', // 存储的 key，默认是 store 的 id
-                storage: localStorage, // 使用 localStorage
-            },
-        ],
-    },
+    })
 });
